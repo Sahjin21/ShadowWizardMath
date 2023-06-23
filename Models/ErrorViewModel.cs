89d2d6f391cf802dca2af59fa80278e7a93dd368
kerds <kerds@DESKTOP-EE673R5>
2023-06-23 18:08:11 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShadowWizardMath.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
