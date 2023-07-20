7fe27bb5fe1e9e679e6304f9c4a4c38b4be91ab9
shawnembry15@gmail.com <shawnembry15@gmail.com>
2023-07-20 13:20:25 -0400

    Added prompts for question and answer boxes along with images. Created MathProblems class.

---
+++ Models/MathProblems.cs
@@ -0,0 +1,44 @@
+﻿namespace ShadowWizardMath.Models
+{
+    public class MathProblems
+    {
+        public int GenerateOperand()
+        {
+            // Generate a random number between 1 and 10
+            Random random = new Random();
+            return random.Next(1, 11);
+        }
+
+        public char GenerateOperator()
+        {
+            // Generate a random operator: '+' for addition, '-' for subtraction
+            Random random = new Random();
+            char[] operators = { '+', '-' };
+            return operators[random.Next(0, 2)];
+        }
+
+        public int CalculateAnswer(int operand1, char @operator, int operand2)
+        {
+            // Calculate the answer based on the operator
+            switch (@operator)
+            {
+                case '+':
+                    return operand1 + operand2;
+                case '-':
+                    return operand1 - operand2;
+                default:
+                    throw new ArgumentException("Invalid operator");
+            }
+        }
+
+        public void GenerateMathProblem(out int operand1, out int operand2, out string operatorString, out int correctAnswer)
+        {
+            operand1 = GenerateOperand();
+            operand2 = GenerateOperand();
+            char @operator = GenerateOperator();
+            operatorString = @operator.ToString();
+            correctAnswer = CalculateAnswer(operand1, @operator, operand2);
+        }
+
+    }
+}
